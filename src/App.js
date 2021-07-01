caaba73ad32aa9dc13b6fd69a33a65749bdade83
Manvi <manvi.jain0@gmail.com>
2021-07-01 11:36:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-// import TodoApp from "./components/todoApp";
-import Main from "./Grocery-bud/Main";
+import TodoApp from "./components/todoApp";
+// import Main from "./Grocery-bud/Main";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <Main />;
+  return (
+    <Router>
+      <Route path="/" component={TodoApp} />
+    </Router>
+  );
 }
 
 export default App;
