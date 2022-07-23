6ae44e54f2ceb2715cbf82a565d007e74e318b18
Manvi <manvi@kawa.space>
2022-07-23 19:42:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import TodoApp from "./components/todoApp";
-// import Main from "./Grocery-bud/Main";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import TodoApp from './components/todoApp';
+import Main from './Grocery-bud/Main';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Route path="/" component={TodoApp} />
-    </Router>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<TodoApp />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
